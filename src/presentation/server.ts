24dca17fa8633348c0d5f04920d8ea10adec0797
J-C-M-D <theprideofthesinner@gmail.com>
2025-03-12 22:07:07 -0500

    Operations c.r.u.d added

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,8 +1,9 @@
-import express from 'express';
+import express, { Router } from 'express';
 import path from 'path';
 
 interface Options {
     port: number;
+    routes: Router;
     public_path?: string;
 }
 
@@ -11,24 +12,29 @@ export class Server {
     private app = express();
     private readonly port: number;
     private readonly public_path: string;
+    private readonly routes: Router;
 
     constructor( options:Options ){
 
-        const { port, public_path = 'public' } = options;
+        const { port, routes, public_path = 'public' } = options;
         this.port = port;
         this.public_path = public_path;
-
+        this.routes = routes
     }
 
     async start() {
 
         //*Middelewares
-      // ambos directorios manejan el mismo concepto
+        this.app.use( express.json() ) //raw
+        this.app.use( express.urlencoded( { extended: true } )); // x-www-form-urlencoded
+
         //*Public folder
-
-
         this.app.use( express.static( this.public_path ) );
 
+        // * Routes
+        this.app.use( this.routes );
+
+        //* SPA
         this.app.get( '*', (request, response) => {
             const indexPath = path.join(__dirname +  `../../../${ this.public_path }/index.html`);
 
@@ -38,8 +44,6 @@ export class Server {
         this.app.listen(this.port, () => {
             console.log(`Server running on port ${ this.port }`)
         })
-
-
     }
 
 } 
